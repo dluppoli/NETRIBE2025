74e89df026d229808c522e9b81a72b9031721cdd
Davide Luppoli <davide@davideluppoli.it>
2025-10-15 18:34:49 +0200

    Esercizio 5 - Javascript cicli e array

---
+++ esercizi/Esercizio 5/README.md
@@ -0,0 +1,3 @@
+# Esercizio 5 - Javascript cicli e array
+
+Seguire le istruzioni contenute nel file script.js
---
+++ esercizi/Esercizio 5/script.js
@@ -0,0 +1,32 @@
+//Dato il seguente array di stringhe, svolgere i seguenti 3 esercizi
+const cartoonCharacters = [
+  "Mickey Mouse", "Donald Duck", "Goofy", "Minnie Mouse", "Daisy Duck",
+  "Pluto", "Bugs Bunny", "Daffy Duck", "Porky Pig", "Elmer Fudd",
+  "Tweety", "Sylvester", "Road Runner", "Wile E. Coyote", "Yosemite Sam",
+  "Tom", "Jerry", "Scooby-Doo", "Shaggy", "Fred",
+  "Daphne", "Velma", "SpongeBob SquarePants", "Patrick Star", "Squidward Tentacles",
+  "Mr. Krabs", "Sandy Cheeks", "Plankton", "Gary", "Popeye",
+  "Olive Oyl", "Bluto", "Betty Boop", "Felix the Cat", "Richie Rich",
+  "Casper", "The Pink Panther", "Inspector Gadget", "George Jetson", "Jane Jetson",
+  "Elroy Jetson", "Judy Jetson", "Astro", "Fred Flintstone", "Wilma Flintstone",
+  "Barney Rubble", "Betty Rubble", "Bamm-Bamm Rubble", "Pebbles Flintstone", "Yogi Bear",
+  "Boo-Boo Bear", "Cindy Bear", "Huckleberry Hound", "Top Cat", "Snagglepuss",
+  "Magilla Gorilla", "Winnie the Pooh", "Tigger", "Piglet", "Eeyore",
+  "Christopher Robin", "Kanga", "Roo", "Owl", "Gopher",
+  "Baloo", "King Louie", "Bagheera", "Shere Khan", "Kaa",
+  "Mowgli", "Peter Pan", "Tinker Bell", "Captain Hook", "Smee",
+  "Ariel", "Flounder", "Sebastian", "Ursula", "Simba",
+  "Mufasa", "Scar", "Timon", "Pumbaa", "Aladdin",
+  "Jasmine", "Genie", "Jafar", "Abu", "Rajah",
+  "Hercules", "Megara", "Hades", "Phil", "Tarzan",
+  "Jane", "Clayton", "Mulan", "Mushu", "Shang",
+  "Stitch", "Lilo", "Angel", "Jumba", "Pleakley"
+];
+
+//1 - Determinare se c'è un personaggio il cui nome completo è più corto di 5 caratteri (stampare true o false)
+
+//2 - Calcolare la somma delle lunghezze dei nomi dei personaggi (stampare il numero a video)
+
+//3 - Identificare e stampare a video il personaggio con il nome più lungo
+
+//4 - Stampare tutti i personaggi della famiglia  Flintstone

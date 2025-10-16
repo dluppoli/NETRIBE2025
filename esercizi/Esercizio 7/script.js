ddaeb22865a5ca1935c78aba96c6965a52901fee
Davide Luppoli <davide@davideluppoli.it>
2025-10-16 16:13:07 +0200

    Esercizio 7 - Lambda Expressions

---
+++ esercizi/Esercizio 7/script.js
@@ -0,0 +1,50 @@
+//Dato il seguente array di stringhe, svolgere i seguenti esercizi UTILIZZANDO LE LAMBDA EXPRESSION
+let cartoonCharacters = [
+  "Mickey Mouse", "Donald Duck", "Goofy", "Minnie Mouse", "Daisy Duck",
+  "Pluto", "Bugs Bunny", "Daffy Duck", "Porky Pig", "Elmer Fudd",
+  "Tweety", "Sylvester", "Road Runner", "Wile E. Coyote", "Yosemite Sam",
+  "Tom", "Jerry", "Scooby-Doo", "Shaggy", "Fred",
+  "Daphne", "Velma", "SpongeBob SquarePants", "Patrick Star", "Squidward Tentacles",
+  "Mr. Krabs", "Sandy Cheeks", "Plankton", "Gary", "Popeye",
+  "Olive Oyl", "Bluto", "Betty Boop", "Felix the Cat", "Richie Rich",
+  "Casper", "The Pink Panther", "Inspector Gadget", "George Jetson", "Jane Jetson",
+  "Elroy Jetson", "Judy Jetson", "Astro", "Fred Flintstone", "Wilma Flintstone",
+  "Barney Rubble", "Betty Rubble", "Bamm-Bamm Rubble", "Pebbles Flintstone", "Yogi Bear",
+  "Boo-Boo Bear", "Cindy Bear", "Huckleberry Hound", "Top Cat", "Snagglepuss",
+  "Magilla Gorilla", "Winnie the Pooh", "Tigger", "Piglet", "Eeyore",
+  "Christopher Robin", "Kanga", "Roo", "Owl", "Gopher",
+  "Baloo", "King Louie", "Bagheera", "Shere Khan", "Kaa",
+  "Mowgli", "Peter Pan", "Tinker Bell", "Captain Hook", "Smee",
+  "Ariel", "Flounder", "Sebastian", "Ursula", "Simba",
+  "Mufasa", "Scar", "Timon", "Pumbaa", "Aladdin",
+  "Jasmine", "Genie", "Jafar", "Abu", "Rajah",
+  "Hercules", "Megara", "Hades", "Phil", "Tarzan",
+  "Jane", "Clayton", "Mulan", "Mushu", "Shang",
+  "Stitch", "Lilo", "Angel", "Jumba", "Pleakley"
+];
+
+
+
+//1 - Ordinare in ordine alfabetico decrescente (e stampare su video)
+
+//2 - Ordinare per lunghezza della stringa crescente (e stampare a video)
+
+//3 - Determinare se c'è un personaggio il cui nome completo è più corto di 5 caratteri (stampare true o false)
+
+//4 - Stampare tutti i personaggi della famiglia Flintstone
+
+//5 - Creare un nuovo array che contiene tutti i nomi in lettere maiuscole (e stampare a video)
+
+//6 - Calcolare la somma delle lunghezze dei nomi dei personaggi (usando reduce)
+
+//7 - Identificare e stampare a video il personaggio con il nome più lungo
+
+
+
+
+
+
+
+
+
+

e84533d420433faeb4ecbadafedb680bc1b4a31c
Davide Luppoli <davide@davideluppoli.it>
2025-10-16 12:55:45 +0200

    Eratostene con funzioni

--- esercizi/Esercizio 6/script.js
+++ esercizi/Esercizio 6/script.js
@@ -1,45 +1,52 @@
 //Dato un numero n
-let n = 50
+
 
 //Stampare tutti i numeri primi <= n
 //Risolvere il problema implementando l'algoritmo di Eratostene (Crivello di Eratostene)
 
 /*Versione 1 - NON OTTIMIZZATA*/
-let numeri = []
-for(let i = 2; i<=n ; i++)
-    numeri.push(i)
-
-for( let i=0; i<numeri.length-1; i++)
+function eratostene_v1(n)
 {
-    if(numeri[i]==undefined) continue;
+    let numeri = []
+    for(let i = 2; i<=n ; i++)
+        numeri.push(i)
 
-    for(let j=i+1; j<numeri.length; j++)
+    for( let i=0; i<numeri.length-1; i++)
     {
-        if( numeri[j]!=undefined && numeri[j] % numeri[i] == 0 ) numeri[j] = undefined
-    }
-}
+        if(numeri[i]==undefined) continue;
 
-for(let i=0; i<numeri.length; i++)
-{
-    if( numeri[i]!=undefined) console.log(numeri[i])
+        for(let j=i+1; j<numeri.length; j++)
+        {
+            if( numeri[j]!=undefined && numeri[j] % numeri[i] == 0 ) numeri[j] = undefined
+        }
+    }
+
+    return numeri
 }
 
 /*Versione 2*/
-numeri = [true,true]
-for(let i = 2; i<=n ; i++)
-    numeri.push(false)
-
-for( let i=2; i<= Math.sqrt(n); i++)
+function eratostene_v2(n)
 {
-    if(numeri[i]==true) continue;
+    let numeri = [true,true]
+    for(let i = 2; i<=n ; i++)
+        numeri.push(false)
 
-    for(let j=i*2; j<numeri.length; j+=i)
+    for( let i=2; i<= Math.sqrt(n); i++)
     {
-        if( numeri[j]!=true && j%i == 0 ) numeri[j] = true
+        if(numeri[i]==true) continue;
+
+        for(let j=i*2; j<numeri.length; j+=i)
+        {
+            if( numeri[j]!=true && j%i == 0 ) numeri[j] = true
+        }
     }
+    return numeri;
 }
 
-for(let i=0; i<numeri.length; i++)
+let n = 50
+let risultati = eratostene_v2(n)
+for(let i=0; i<=risultati.length; i++)
 {
-    if( numeri[i]!=true) console.log(i)
+    if( risultati[i]==false) console.log(i)
 }
+
